93581126c971da6302a36d00465043f9e831daf3
Parivesh Mehta <mehtaparivesh@gmail.com>
2022-09-22 19:47:40 +0530

    Primes and multipliation done

---
+++ C_Primes_and_Multiplication.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define pb emplace_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for (int i = (a); i <= (b); i++)
+#define looprev(i, a, b) for (int i = (a); i >= (b); i--)
+#define logarr(arr, a, b)            \
+    for (int z = (a); z <= (b); z++) \
+        cout << (arr[z]) << " ";     \
+    cout << endl;
+
+// typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ll power(long long x, ll y, ll p)
+{
+    ll res = 1;
+    x = x % p;
+    if (x == 0)
+        return 0;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+long long modInverse(unsigned long long n,
+                     int p)
+{
+    return power(n, p - 2, p);
+}
+
+unsigned long long nCrModPFermat(unsigned long long n,
+                                 int r, int p)
+{
+    if (n < r)
+        return 0;
+    if (r == 0)
+        return 1;
+    long long fac[n + 1];
+    fac[0] = 1;
+    for (int i = 1; i <= n; i++)
+        fac[i] = (fac[i - 1] * i) % p;
+    return (fac[n] * modInverse(fac[r], p) % p * modInverse(fac[n - r], p) % p) % p;
+}
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main(int argc, char const *argv[])
+{
+    file_i_o();
+    ll x, n;
+    cin >> x >> n;
+    ll ans = 1;
+    vi primes;
+    for (int i = 2; i * i <= x; i++)
+    {
+        if (x % i == 0)
+            primes.push_back(i);
+        while (x % i == 0)
+        {
+            x /= i;
+        }
+    }
+    if (x > 1)
+        primes.push_back(x);
+    for (auto p : primes)
+    {
+        ll temp = n;
+        ll exponent = 0;
+
+        while (temp)
+        {
+
+            exponent += (temp / p);
+            temp /= p;
+        }
+
+        ans *= power(p, exponent, mod);
+        ans %= mod;
+    }
+    cout << ans << endl;
+    return 0;
+}
